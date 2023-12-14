942041abda4a7302cba534b662f8d1e7f3999a13
rockyhg <hrk.haga@gmail.com>
2023-12-14 23:23:19 +0900

    位置調整

--- app.py
+++ app.py
@@ -18,40 +18,42 @@ def main():
     st.set_page_config(page_title="Lightning Talk Timer", page_icon="⏰")
     st.title("Lightning Talk Timer")
 
+    # セッションステートの初期化
     if "running" not in st.session_state:
         st.session_state.running = False
+    if "minutes" not in st.session_state:
+        st.session_state.minutes = 0
+    if "seconds" not in st.session_state:
+        st.session_state.seconds = 0
 
     # カラムの作成
     cols = st.columns(2)
 
-    # カラムにウィジェットを配置
-    with cols[0]:
-        timer_minutes = st.number_input("Minutes:", value=1, min_value=0, max_value=60)
-    with cols[1]:
-        timer_seconds = st.number_input(
-            "Seconds:", value=0, min_value=0, max_value=59, step=10
-        )
+    # Inputウィジェットを配置
+    timer_minutes = st.number_input("Minutes:", value=1, min_value=0, max_value=60)
+    timer_seconds = st.number_input(
+        "Seconds:", value=0, min_value=0, max_value=59, step=10
+    )
 
-    # 合計タイマー時間（秒）を計算
+    # 合計タイマー時間(秒)を計算
     total_seconds = timer_minutes * 60 + timer_seconds
 
-    # 設定時間と表示する場所を確保
+    # 設定時間を表示する場所を確保
     time_display = st.empty()
 
     # タイマー開始とリセットボタン
-    start_button, reset_button = st.columns(2)
+    start_button, reset_button = st.columns([1, 7])
 
     if start_button.button("START"):
         st.session_state.running = True
 
     if reset_button.button("RESET"):
         st.session_state.running = False
+        st.session_state.minutes = 0
+        st.session_state.seconds = 0
 
-    # 画像表示のためのカラム
-    col_left, col_image, col_right = st.columns([1, 2, 1])
-    with col_image:
-        # 画像表示のためのプレースホルダー
-        image_placeholder = st.empty()
+    # 画像表示のためのプレースホルダー
+    image_placeholder = st.empty()
 
     # タイマー実行
     if st.session_state.running:
